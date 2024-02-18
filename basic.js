7a91b67cb3f4cec2b00fec3e8d84bfe1b3fdf19f
yunabeza <lolita.inabeza@gmail.com>
2024-02-17 23:47:49 -0500

    Made the input fields reactive to click actions by changing their box color + correction of the home's background-image view

--- basic.js
+++ basic.js
@@ -5,3 +5,28 @@ function openService(){
 function openBooking(){
     window.location.href = "#booking";
 }
+
+// code inspired by: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_focus_blur
+//jquery to change input field color when its active
+$(document).ready(function(){
+    $("input").focus(function(){
+        $(this).css("background-color", "#fbfbf0")
+    });
+    $("input").blur(function(){
+        $(this).css("background-color", "white")
+    });
+
+    $("textarea").focus(function(){
+        $(this).css("background-color", "#fbfbf0")
+    });
+    $("textarea").blur(function(){
+        $(this).css("background-color", "white")
+    });
+
+    $("select").focus(function(){
+        $(this).css("background-color", "#fbfbf0")
+    });
+    $("select").blur(function(){
+        $(this).css("background-color", "white")
+    });
+  });
